becdd84536774edde93dfffb63b76439db4f94ba
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-11-29 18:57:05 -0600

    Agregar funcionalidad de inicio de sesión y pantalla de éxito tras el inicio de sesión

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
+    def login(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+        if username in users and users[username]['password'] == password:
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong username or password!"
         
 
 class RootWidget(ScreenManager):
@@ -29,6 +37,12 @@ class SignUpScreenSuccess(Screen):
     def go_to_login(self):
         self.manager.transition.direction = 'right'
         self.manager.current = "login_screen"
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
 
 class MainApp(App):
     def build(self):
